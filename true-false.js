4658130f6a7a918354784fef82fed91ca3545812
iqbal-dev <pkmdiqbal@gmail.com>
2020-11-05 00:46:32 +0600

    advance javascript

---
+++ true-false.js
@@ -0,0 +1,18 @@
+//falsy value
+//""
+//0
+//-0
+//false
+//null
+//undefined
+//NaN
+
+
+
+var name = '';
+if (-0) {
+    console.log('condition is true');
+}
+else {
+    console.log('condition is false');
+}
