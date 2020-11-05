ba125892898f060b6480d8926c9bb88f9e37a845
iqbal-dev <pkmdiqbal@gmail.com>
2020-11-05 12:24:57 +0600

    double vs triple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,23 @@
+
+let first = 2;
+let second = "2";
+
+/*check with double equal*/
+if (first == second) {
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
+//output "condition is true"
+
+
+/*check with triple equal*/
+
+if (first === second) {
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
+//here output "condition is false"
