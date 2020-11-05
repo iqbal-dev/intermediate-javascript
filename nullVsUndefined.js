4407f2757ee2cb8f1ba94935d867f4d0e80cb9ce
iqbal-dev <pkmdiqbal@gmail.com>
2020-11-05 12:12:26 +0600

    nul vs undefined

---
+++ nullVsUndefined.js
@@ -0,0 +1,25 @@
+
+/*****undefined*****/
+var pakhi;
+console.log(pakhi);  //here pakhi is undefined
+
+function add(num1,num2) {
+    console.log(num1,num2);//here num1 is 13 and num2 is undefined
+}
+add(13)
+
+const obj = {
+    name: 'rabin',
+    number: 011245
+}
+console.log(obj.name)//output is rabin
+console.log(obj.girlFriendlyName)//here girlFriendlyName property is undefined
+
+const arr = [1, 2, 3, 4];
+console.log(arr[10])//here arr[10] is undefined because arr[10] is not available in arr.
+
+const number = undefined;
+console.log(number)
+
+/*****Null*****/
+//null is an object which is it to the variable
